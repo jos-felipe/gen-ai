507cb20d987f0090796e9aebf56d6215d69b1758
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-09-24 13:01:17 -0300

    functional

--- project2/ex01/loading.py
+++ project2/ex01/loading.py
@@ -1,52 +1,44 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    loading.py                                         :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: josfelip <josfelip@student.42sp.org.br>    +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2020/05/26 17:00:36 by frfrey            #+#    #+#              #
-#    Updated: 2024/09/23 16:27:20 by josfelip         ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import time
 from time import sleep
 
-
 def ft_progress(lst):
     start_time = time.time()
-    iter_time = 0
-    for i in lst:
-        if i == 1:
-            iter_time = time.time() - start_time
-        filled_length = int(i * 25 / len(lst))
-        bar = '#' * filled_length + '-' * (25 - filled_length)
-        
+    total_length = len(lst)  # Armazena o comprimento total da lista
+
+    for i, elem in enumerate(lst, start=1):  # Começa a contagem de 1
+        current_time = time.time()  # Captura o tempo atual a cada iteração
+        elapsed_time = current_time - start_time  # Tempo decorrido
+        if i > 1:  # Para evitar divisão por zero
+            iter_time = elapsed_time / i  # Tempo médio por iteração
+
+        filled_length = int(i * 25 / total_length)  # Cálculo da parte preenchida da barra
+        bar = '#' * filled_length + '-' * (25 - filled_length)  # Cria a barra
+
+        # Cálculo da ETA
+        eta = (total_length - i) * iter_time if i > 1 else 0
+
+        # Impressão da barra de progresso
         print(
             "\rETA: {:5.2f} [{:>4.0%}] [{}] {}/{} | elapsed time {:.2f}s".format(
-                (len(lst) - i) * iter_time,
-                i / len(lst),
+                eta,
+                i / total_length,
                 bar,
-                i + 1,
-                len(lst),
-                time.time() - start_time
+                i,
+                total_length,
+                elapsed_time
             ),
             end=""
         )
-        yield i
-
-
+        yield elem  # Use 'elem' para manter a lógica intacta
+        sleep(0.01)  # Simula um pequeno atraso
 
 def main():
-	a_list = range(1000)
-	ret = 0
-	for elem in ft_progress(a_list):
-		ret += (elem + 3) % 5
-		sleep(0.01)
-	print()
-	print(ret)
-
+    a_list = range(1000)
+    ret = 0
+    for elem in ft_progress(a_list):
+        ret += (elem + 3) % 5
+    print()  # Para quebrar a linha após a barra de progresso
+    print(ret)  # Exibe o resultado final
 
 if __name__ == "__main__":
     main()
