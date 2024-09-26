41f633efa182c3995dfd470f760e2e41586e00c9
Jose Felipe Dalcin Stieven <josfelip@c2r3p1.42sp.org.br>
2024-09-26 09:45:36 -0300

    be_strcutred.py done

--- project3/ex01/be_structured.py
+++ project3/ex01/be_structured.py
@@ -1,72 +1,190 @@
-import re
+import os
+import requests
+import json
 
-def format_prompt(descricao_emprego: str) -> str:
-    """
-    Gera um prompt formatado com base em uma descrição de emprego para ser utilizado em modelos de IA.
+# Configurações das APIs
+GROQ_API_KEY = os.environ.get("GROQ_API_KEY")
+GEMINI_API_KEY = os.environ.get("GEMINI_API_KEY")
+
+
+def format_prompt(document: str):
+    # return zero_shot_prompt(document)
+    return few_shot_prompt(document)
+
+
+def zero_shot_prompt(document: str):
+    """Formata a descrição da vaga em um zero shot prompt estruturado"""
+
+    prompt = f"""Analise a seguinte descrição de emprego e extraia as seguintes informações em um formato estruturado:
     
-    :param descricao_emprego: Uma string contendo a descrição completa do emprego.
-    :return: Uma string formatada com as principais seções extraídas da descrição do emprego.
+    * **Cargo:**
+    * **Empresa:**
+    * **Localização:**
+    * **Responsabilidades Principais:** (liste no máximo 5)
+    * **Qualificações Necessárias:** (liste no máximo 5)
+    * **Salário:** (se mencionado)
+    * **Benefícios:** (se mencionados)
+    
+    **Descrição do Emprego:**
+    ```
+    {document}
+    ```
     """
-    # Dicionário para armazenar as seções formatadas
-    secoes_formatadas = {
-        'Job Title': '',
-        'Location': '',
-        'About The Job': '',
-        'Minimum Qualifications': '',
-        'Preferred Qualifications': '',
-        'Responsibilities': ''
+    return prompt
+
+
+def few_shot_prompt(document: str):
+    """Formata a descrição da vaga em um few shot prompt estruturado"""
+
+    prompt = f"""Analise a seguinte descrição de emprego e extraia as seguintes informações em um formato estruturado:
+    
+    **Exemplo 1:**
+
+    * **Descrição do Emprego:** "Estamos buscando um Desenvolvedor de Software experiente para se juntar à nossa equipe em São Paulo. Você será responsável por projetar, desenvolver e testar novas funcionalidades para nosso aplicativo móvel. Precisamos de alguém com fortes habilidades em Java e experiência com desenvolvimento Android. Oferecemos um salário competitivo e um excelente pacote de benefícios."
+
+    * **Informações Extraídas:**
+        * Cargo: Desenvolvedor de Software
+        * Empresa: Não mencionada
+        * Localização: São Paulo
+        * Responsabilidades Principais:
+            * Projetar novas funcionalidades
+            * Desenvolver funcionalidades
+            * Testar funcionalidades
+        * Qualificações Necessárias:
+            * Fortes habilidades em Java
+            * Experiência com desenvolvimento Android
+        * Salário: Competitivo
+        * Benefícios: Excelente pacote
+
+    **Exemplo 2:**
+
+    * **Descrição do Emprego:** "Procuramos um Analista de Marketing Digital para criar e gerenciar campanhas de marketing online. O candidato ideal terá experiência com ferramentas de análise de dados e mídias sociais. Esta é uma posição remota com salário a combinar."
+
+    * **Informações Extraídas:**
+        * Cargo: Analista de Marketing Digital
+        * Empresa: Não mencionada
+        * Localização: Remota
+        * Responsabilidades Principais:
+            * Criar campanhas de marketing online
+            * Gerenciar campanhas de marketing online
+        * Qualificações Necessárias:
+            * Experiência com ferramentas de análise de dados
+            * Experiência com mídias sociais
+        * Salário: A combinar
+        * Benefícios: Não mencionados
+    
+    **Agora, analise a seguinte descrição de emprego e extraia as mesmas informações:**
+    ```
+    {document}
+    ```
+    """
+    return prompt
+
+
+def process_gemini(content: dict):
+    return content["candidates"][0]["content"]["parts"][0]["text"]
+
+
+def process_groq(content: dict):
+    return content["choices"][0]["message"]["content"]
+
+
+def process_qwen2(content: bytes):
+    lines = content.decode("utf-8").splitlines()
+
+    response_parts = []
+
+    for line in lines:
+        try:
+            data = json.loads(line)
+            if data.get("done"):
+                break
+            response_parts.append(data["response"])
+        except json.JSONDecodeError:
+            print(f"Erro ao decodificar JSON: {line}")
+
+    full_response = "".join(response_parts)
+    return full_response
+
+
+def query_gemini(prompt: str):
+    print("Consultando Gemini...")
+    try:
+        url = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={GEMINI_API_KEY}"
+        headers = {"Content-Type": "application/json"}
+        data = {"contents": [{"parts": [{"text": prompt}]}]}
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise requests.exceptions.RequestException(
+            f"Erro na requisição da API do Gemini: {e}"
+        )
+
+
+def query_groq(prompt: str):
+    print("Consultando Groq...")
+    try:
+        url = f"https://api.groq.com/openai/v1/chat/completions"
+        headers = {
+            "Authorization": f"Bearer {GROQ_API_KEY}",
+            "Content-Type": "application/json",
+        }
+        data = {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": prompt,
+                }
+            ],
+            "model": "llama3-8b-8192",
+        }
+        response = requests.post(url, json=data, headers=headers)
+        response.raise_for_status()
+        return response.json()
+    except requests.exceptions.RequestException as e:
+        raise requests.exceptions.RequestException(
+            f"Erro na requisição da API do Groq: {e}"
+        )
+
+
+def query_qwen2(prompt: str):
+    print("Consultando Ollama...")
+    try:
+        url = "http://localhost:11434/api/generate"
+        data = {
+            "model": "qwen2:1.5b",
+            "prompt": prompt,
+        }
+        response = requests.post(url, json=data)
+        response.raise_for_status()
+        return response.content
+    except requests.exceptions.RequestException as e:
+        raise requests.exceptions.RequestException(
+            f"Erro na requisição da API do Ollama: {e}"
+        )
+
+
+def query_all_models(prompt: str):
+    """Consulta todos os modelos e retorna as respostas."""
+    return {
+        "Gemini_1.5_Flash": process_gemini(query_gemini(prompt)),
+        "Groq_Llama3_8b_8192": process_groq(query_groq(prompt)),
+        "Ollama_Qwen2:1.5b": process_qwen2(query_qwen2(prompt)),
     }
 
-    # Expressões regulares para capturar as partes da descrição
-    titulo_regex = r'(Software Engineer, Early Career)'
-    localizacao_regex = r'(Greater São Paulo Area|Belo Horizonte|São Paulo|State of São Paulo|State of Minas Gerais)'
-    sobre_o_trabalho_regex = r'About The Job(.*?)Responsibilities'
-    qualificacoes_minimas_regex = r'Minimum qualifications:(.*?)(?:Preferred qualifications|$)'
-    qualificacoes_preferidas_regex = r'Preferred qualifications:(.*?)(?:About The Job|Responsibilities|$)'
-    responsabilidades_regex = r'Responsibilities(.*?)(?:Google is proud to be an equal opportunity workplace|$)'
-
-    # Capturar as seções relevantes
-    titulo_match = re.search(titulo_regex, descricao_emprego, re.IGNORECASE | re.DOTALL)
-    if titulo_match:
-        secoes_formatadas['Job Title'] = titulo_match.group(1).strip()
-
-    localizacao_match = re.search(localizacao_regex, descricao_emprego, re.IGNORECASE | re.DOTALL)
-    if localizacao_match:
-        secoes_formatadas['Location'] = localizacao_match.group(1).strip()
-
-    sobre_o_trabalho_match = re.search(sobre_o_trabalho_regex, descricao_emprego, re.IGNORECASE | re.DOTALL)
-    if sobre_o_trabalho_match:
-        secoes_formatadas['About The Job'] = sobre_o_trabalho_match.group(1).strip()
-
-    qualificacoes_minimas_match = re.search(qualificacoes_minimas_regex, descricao_emprego, re.IGNORECASE | re.DOTALL)
-    if qualificacoes_minimas_match:
-        secoes_formatadas['Minimum Qualifications'] = qualificacoes_minimas_match.group(1).strip()
-
-    qualificacoes_preferidas_match = re.search(qualificacoes_preferidas_regex, descricao_emprego, re.IGNORECASE | re.DOTALL)
-    if qualificacoes_preferidas_match:
-        secoes_formatadas['Preferred Qualifications'] = qualificacoes_preferidas_match.group(1).strip()
-
-    responsabilidades_match = re.search(responsabilidades_regex, descricao_emprego, re.IGNORECASE | re.DOTALL)
-    if responsabilidades_match:
-        secoes_formatadas['Responsibilities'] = responsabilidades_match.group(1).strip()
-
-    # Formatar o prompt para IA
-    prompt = (
-        f"Job Title: {secoes_formatadas['Job Title']}\n"
-        f"Location: {secoes_formatadas['Location']}\n\n"
-        f"About The Job:\n{secoes_formatadas['About The Job']}\n\n"
-        f"Minimum Qualifications:\n{secoes_formatadas['Minimum Qualifications']}\n\n"
-        f"Preferred Qualifications:\n{secoes_formatadas['Preferred Qualifications']}\n\n"
-        f"Responsibilities:\n{secoes_formatadas['Responsibilities']}\n"
-    )
-
-    return prompt.strip()
 
 def main():
     with open("job_description.txt", "r") as file:
         job_description = file.read()
-        formatted_prompt = format_prompt(job_description)
-        print(formatted_prompt)
+
+    formatted_prompt = format_prompt(job_description)
+    results = query_all_models(formatted_prompt)
+    for model, response in results.items():
+        print(f"\nAnálise do {model}:")
+        print(response)
+        print("-" * 50)
+
 
 if __name__ == "__main__":
-	main()
+    main()
